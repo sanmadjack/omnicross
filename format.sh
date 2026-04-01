d4e77cc3d017d2aa9a02048b9d21490c6573da8d
Matthew Barbour <sanmadjack@gmail.com>
2026-04-01 16:06:48 -0500

    updated format script to check for success and to use the new compilations property

--- format.sh
+++ format.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-jq --tab -j '.|=sort_by(.title)' data.json > data.tmp
-mv data.tmp data.json
+jq --tab -j '.compilations|sort_by(.title)' data.json > data.tmp
+if [ $? -eq 0 ]; then
+    mv data.tmp data.json
+fi
