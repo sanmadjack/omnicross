cf848feb43c9e3fb216767ca07c7d28f1a53f31b
Matthew Barbour <sanmadjack@gmail.com>
2026-04-01 16:18:55 -0500

    corrected json sort

--- format.sh
+++ format.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-jq --tab -j '.compilations|sort_by(.title)' data.json > data.tmp
+jq --tab -j '.compilations|=sort_by(.title)' data.json > data.tmp
 if [ $? -eq 0 ]; then
     mv data.tmp data.json
 fi
